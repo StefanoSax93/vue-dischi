cd3991410c6038c08eed9c9cb3e5ff8fcd4b4743
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-25 21:08:17 +0200

    aggiunta select artista

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -1,11 +1,19 @@
 <template>
         <div class="bg_primary flex-grow-1">
             <div class="container px-5 mt-5">
-                <div class="text-end mb-5">
-                    <select name="" id="genreSelect" v-model="SelectedGenre" class="rounded py-1" style="width: 100px;">
-                        <option selected value="">Genere</option>
-                        <option :value="genre" v-for="(genre,id) in GenresList" :key="id">{{genre}}</option>
-                    </select>
+                <div class="d-flex justify-content-between mb-5">
+                    <div>
+                        <select name="" id="authorSelect" v-model="SelectedAuthor" class="rounded py-1" style="width: 100px;">
+                            <option selected value="">Artista</option>
+                            <option :value="author" v-for="(author,id) in AuthorList" :key="id">{{author}}</option>
+                        </select>
+                    </div>
+                    <div>
+                        <select name="" id="genreSelect" v-model="SelectedGenre" class="rounded py-1" style="width: 100px;">
+                            <option selected value="">Genere</option>
+                            <option :value="genre" v-for="(genre,id) in GenresList" :key="id">{{genre}}</option>
+                        </select>
+                    </div>
                 </div>
                 <div class="row row-cols-5 gx-5">
                     <div class="col mb-4" v-for="song in filterAlbums" :key="song.author">
@@ -29,15 +37,18 @@ export default {
             apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
             SongsList: [],
             GenresList: [],
+            AuthorList: [],
             SelectedGenre: '',
+            SelectedAuthor: '',
         }
     },
     
     computed: {
         filterAlbums() {
             const filteredAlbums = [];
+
             this.SongsList.forEach(song => {
-                if (song.genre.includes(this.SelectedGenre)) {
+                if (song.genre.includes(this.SelectedGenre) && song.author.includes(this.SelectedAuthor)) {
                     filteredAlbums.push(song);
                 }
             });
@@ -54,6 +65,11 @@ export default {
                     this.SongsList.forEach(song => {
                         if (!this.GenresList.includes(song.genre)) {
                             this.GenresList.push(song.genre);
+                        }
+                    });
+                    this.SongsList.forEach(song => {
+                        if (!this.AuthorList.includes(song.author)) {
+                            this.AuthorList.push(song.author);
                         }
                     });
                 })
